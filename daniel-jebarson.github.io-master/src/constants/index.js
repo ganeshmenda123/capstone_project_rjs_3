6bfe4886eeb22dcf2ef7a3456f1eeeff61aa3f48
ganeshmenda123 <21p31a04n0@acet.ac.in>
2023-09-27 21:21:59 +0530

    ganesh

---
+++ daniel-jebarson.github.io-master/src/constants/index.js
@@ -0,0 +1,107 @@
+export const projects = [
+  {
+    title: "wed page design",
+    desc: "I want to be strong in web development and backend development.Strong in design and integration with intuitive problems - problemsolving. Passionate in implementing innovative web pages.",
+    tags: [
+      {
+        name: "HTML",
+        color: "red-600",
+      },
+      {
+        name: "CSS",
+        color: "green-600",
+      },
+      {
+        name: "BootStrap",
+        color: "green-600",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109025615442608168/pvz.png",
+    link: "https://pvz-2-api.vercel.app/",
+    code: "https://github.com/code-cracked/plants-vs-zombies-api",
+  },
+  {
+    title: "app Design",
+    desc: "I want to be strong in web development and backend development.Strong in design and integration with intuitive problems - problemsolving. Passionate in implementing innovative web pages.",
+    tags: [
+      {
+        name: "HTML",
+        color: "red-600",
+      },
+      {
+        name: "CSS",
+        color: "green-600",
+      },
+      {
+        name: "BootStrap",
+        color: "green-600",
+      },
+    ],
+    img: "https://user-images.githubusercontent.com/88134306/227576773-06c3a1d2-0a1a-4ab2-8d1f-e06d71408410.png",
+    link: "https://web-chat-app-brown.vercel.app",
+    code: "https://github.com/daniel-jebarson/web-chat-app",
+  },
+  {
+    title: "web design",
+    desc: "I want to be strong in web development and backend development.Strong in design and integration with intuitive problems - problemsolving. Passionate in implementing innovative web pages.",
+    tags: [
+      {
+        name: "HTML",
+        color: "red-600",
+      },
+      {
+        name: "CSS",
+        color: "green-600",
+      },
+      {
+        name: "BootStrap",
+        color: "green-600",
+      },
+    ],  
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109031291522928670/Exploiter.png",
+    link: "https://drive.google.com/file/d/1npEOuOhQh-1zRiLB9fFn-6ORTwd1gZkl/view",
+    code: "https://github.com/daniel-jebarson/EXPLOITER",
+  },
+  {
+    title: "develop webpage",
+    desc: "I want to be strong in web development and backend development.Strong in design and integration with intuitive problems - problemsolving. Passionate in implementing innovative web pages.",
+    tags: [
+      {
+        name: "HTML",
+        color: "red-600",
+      },
+      {
+        name: "CSS",
+        color: "green-600",
+      },
+      {
+        name: "BootStrap",
+        color: "green-600",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109032447783469097/image.png",
+    link: "https://stock-market-seven.vercel.app/",
+    code: "https://github.com/daniel-jebarson/Stock-Market",
+  },
+  {
+    title: "Forms design",
+    desc: "I want to be strong in web development and backend development.Strong in design and integration with intuitive problems - problemsolving. Passionate in implementing innovative web pages.",
+    tags: [
+      {
+        name: "HTML",
+        color: "red-600",
+      },
+      {
+        name: "CSS",
+        color: "green-600",
+      },
+      {
+        name: "BootStrap",
+        color: "green-600",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109035277814611978/Screenshot_19.png",
+    link: "https://github.com/code-cracked/you-farm",
+    code: "https://github.com/code-cracked/you-farm",
+  },
+];
